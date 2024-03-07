2b797cdece54a4ed9257dc3bd1bd06d8d11857d4
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-07 20:01:39 +0000

    RightSize project, added to sln

---
+++ RightSize/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
