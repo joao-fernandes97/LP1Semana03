7530fce21f02e613dc2211f9be89ad132b817a79
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-07 22:10:17 +0000

    ArrayMult project added to sln

---
+++ ArrayMult/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
