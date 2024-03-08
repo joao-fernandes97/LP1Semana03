f4c5165a8961889103280d18d78631207e93b691
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-08 11:43:41 +0000

    PowTwoUntil project added to sln

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
