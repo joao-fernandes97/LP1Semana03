e2905664cee4c552c5ab360c2e33b75f92b33f96
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-08 00:55:07 +0000

    Load args into matrices

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -6,7 +6,21 @@ namespace ArrayMult
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if(args.Length==6){
+                float[,] matrixA = new float[2,2];
+                matrixA[0,0]=float.Parse(args[0]);
+                matrixA[0,1]=float.Parse(args[1]);
+                matrixA[1,0]=float.Parse(args[2]);
+                matrixA[1,1]=float.Parse(args[3]);
+        
+                float[,] matrixB = new float[1,2];
+                matrixB[0,0]=float.Parse(args[4]);
+                matrixB[0,1]=float.Parse(args[5]);
+
+            } else
+            {
+                Console.WriteLine("Not enough args");
+            }
         }
     }
 }
