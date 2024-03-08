0b59c5f4997c4089cfb7a0b7e17886a9e6a585ff
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-08 12:06:45 +0000

    Modified logic to use the GameStatus enum to handle the result of the game

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -6,37 +6,37 @@ namespace RPS
     {
         private static void Main(string[] args)
         {
-            int result = RockPaperScissors(args[0], args[1]);
+            GameStatus result = RockPaperScissors(args[0], args[1]);
             switch (result)
             {
-                case 0:
+                case GameStatus.Draw:
                     Console.WriteLine("It's a draw!");
                     break;
-                case 1:
+                case GameStatus.Player1Wins:
                     Console.WriteLine("Player 1 wins!");
                     break;
-                case 2:
+                case GameStatus.Player2Wins:
                     Console.WriteLine("Player 2 wins!");
                     break;
             }
         }
 
-        private static int RockPaperScissors(string player1, string player2)
+        private static GameStatus RockPaperScissors(string player1, string player2)
         {
-            int result;
+            GameStatus result;
             if (player1 == player2)
             {
-                result = 0; // Draw
+                result = GameStatus.Draw; // Draw
             }
             if (((player1 == "Rock") && (player2 == "Scissors")) ||
                 ((player1 == "Scissors") && (player2 == "Paper")) ||
                 ((player1 == "Paper") && (player2 == "Rock")))
             {
-                result = 1; // Player 1 wins
+                result = GameStatus.Player1Wins; // Player 1 wins
             }
             else
             {
-                result = 2; // Player 2 wins
+                result = GameStatus.Player2Wins; // Player 2 wins
             }
             return result;
         }
