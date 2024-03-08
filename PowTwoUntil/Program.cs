85a1d8d937a5bdb4ffa3ea06157131074a256426
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-08 11:45:51 +0000

    PowersOf2Until5 method added to program

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -8,5 +8,13 @@ namespace PowTwoUntil
         {
             Console.WriteLine("Hello, World!");
         }
+
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (1<<5); i = i<<1)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
